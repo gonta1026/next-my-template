9b47f7e0a57e91ba0e95b855e84d770f445cd0c0
gonta1026 <katano.original@gmail.com>
2024-07-14 12:40:12 +0900

    fix: 複数ファイルを追加

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,14 +1,7 @@
 import Image from "next/image";
-import Link from "next/link";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  const hoge = {
-    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: true,
-  };
-  const hoiああああああああああああああああああああああああああああああああああああああああああああああge = () => {
-    //
-  };
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -16,6 +9,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
+
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -31,8 +25,6 @@ export default function Home() {
       <div className={styles.center}>
         <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
       </div>
-
-      <Link href={"./sample"}>あああああああああああああああああああああああああああああああああああ</Link>
 
       <div className={styles.grid}>
         <a
